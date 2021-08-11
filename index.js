d84d98fccf0c0711d60ca5308f13197dd900e00b
marcos <marcosrodriguesbrugnaro@gmail.com>
2021-08-11 11:21:57 -0300

    Test app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {name as appName} from './app.json';
@@ -8,10 +8,16 @@ import Routes from './src/config/routes';
 
 function App() {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <View>
+      <Text>test 02</Text>
+    </View>
   );
 }
 
 AppRegistry.registerComponent(appName, () => App);
+
+{
+  /* <NavigationContainer>
+      <Routes />
+    </NavigationContainer> */
+}
