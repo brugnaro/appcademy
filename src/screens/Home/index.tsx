b4f1b684c224d6ffedd2d22797ddbf872adbe599
brugnaro <marcosrodriguesbrugnaro@gmail.com>
2021-09-01 10:16:02 -0300

    feature_1234-alteracao-texto-home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles';
 // https://icons-for-free.com/iconfiles/png/512/agenda+app+contacts+online+profile+user+icon-1320183042135412367.png
 const Home = () => (
   <View style={styles.container}>
-    <Text>home</Text>
+    <Text>home test</Text>
   </View>
 );
 
